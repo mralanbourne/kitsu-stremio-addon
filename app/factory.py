a1cb155ab3ed5ab6afbeff9d6536a77495d63b25
(ᇴ‿ฺᇴ) <spam-born@inbox.lv>
2026-03-16 13:38:21 +0100

    Proper CORS Handling

--- app/factory.py
+++ app/factory.py
@@ -1,24 +1,27 @@
+from quart import request, Response
 from app.app import App
 from app.routes.auth import auth_blueprint
 from app.routes.catalog import catalog_bp
 from app.routes.content_sync import content_sync_bp
 from app.routes.manifest import manifest_blueprint
 from app.routes.ui import ui_bp
-from quart import request, Response 
 
 def create_app() -> App:
     app_ = App(__name__, template_folder="../templates", static_folder="./static")
     app_.config.from_object("config.Config")
     
-    @app_.before_request
-    async def handle_preflight():
+   
+    @app_.errorhandler(405)
+    async def handle_options_preflight(error):
         if request.method == "OPTIONS":
-            resp = Response("")
+            resp = Response("", status=200)
             resp.headers["Access-Control-Allow-Origin"] = "*"
             resp.headers["Access-Control-Allow-Headers"] = "*"
             resp.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
             return resp
+        return "Method Not Allowed", 405
 
+    
     @app_.after_request
     async def add_cors_headers(response):
         response.headers["Access-Control-Allow-Origin"] = "*"
