ab8ce6374eeac91efd421749aa7cbb8b9349a42c
(ᇴ‿ฺᇴ) <spam-born@inbox.lv>
2026-03-17 14:21:52 +0100

    Switch to async MongoDB client and update methods
    Refactor database interactions to use AsyncIOMotorClient for asynchronous operations.

--- app/services/db.py
+++ app/services/db.py
@@ -1,17 +1,15 @@
 import certifi
 from datetime import datetime, timedelta
 from typing import Optional
-from pymongo import MongoClient
-from pymongo.collection import Collection
-from pymongo.database import Database
+from motor.motor_asyncio import AsyncIOMotorClient
 from config import Config
 
-client: MongoClient = MongoClient(Config.MONGO_URI, tlsCAFile=certifi.where())
-db: Database = client.get_database(Config.MONGO_DB)
-UID_map_collection: Collection = db.get_collection(Config.MONGO_UID_MAP)
+client = AsyncIOMotorClient(Config.MONGO_URI, tlsCAFile=certifi.where())
+db = client.get_database(Config.MONGO_DB)
+UID_map_collection = db.get_collection(Config.MONGO_UID_MAP)
 
-def get_user(user_id: str) -> Optional[dict]:
-    return UID_map_collection.find_one(
+async def get_user(user_id: str) -> Optional[dict]:
+    return await UID_map_collection.find_one(
         {"uid": user_id}, 
         {
             "_id": 0, 
@@ -26,23 +24,27 @@ def get_user(user_id: str) -> Optional[dict]:
         }
     )
 
-def store_user(user_details: dict) -> bool:
+async def store_user(user_details: dict) -> bool:
     user_id = user_details["id"]
     user_details["uid"] = user_id
     data = user_details.copy()
     
-    if user := UID_map_collection.find_one({"uid": user_id}, {"_id": 1}):
-        return UID_map_collection.update_one({"_id": user["_id"]}, {"$set": data}).acknowledged
-    return UID_map_collection.insert_one(data).acknowledged
+    user = await UID_map_collection.find_one({"uid": user_id}, {"_id": 1})
+    if user:
+        result = await UID_map_collection.update_one({"_id": user["_id"]}, {"$set": data})
+        return result.acknowledged
+    
+    result = await UID_map_collection.insert_one(data)
+    return result.acknowledged
 
-def update_user_progress(user_id: str, anime_id: str, episode: int):
-    UID_map_collection.update_one(
+async def update_user_progress(user_id: str, anime_id: str, episode: int):
+    await UID_map_collection.update_one(
         {"uid": user_id},
         {"$set": {f"progress.{anime_id}": episode}}
     )
 
-def get_valid_user(user_id: str) -> tuple[dict, Optional[str]]:
-    user = get_user(user_id)
+async def get_valid_user(user_id: str) -> tuple[dict, Optional[str]]:
+    user = await get_user(user_id)
     if not user:
         return {}, "No user found. Please re-login to Kitsu."
         
